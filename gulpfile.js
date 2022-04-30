64556d9c051d2398792d552c2a2e3bff7a3da7ae
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2022-04-29 21:19:09 -0500

    Se crea el gulp, SASS y se hace un waths de todos los documentos para que se guarden a la hora de guardar desde cualquier scss

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+// function tarea(done){//Funciona como done, callback o fn
+// console.log("Desde la primer tarea");
+// done();//Finaliza la tarea
+// }
+// function tarea2 (done){
+//     console.log("Segunda tarea");
+//     done();
+// }
+
+// exports.tarea = tarea;//para exportar y poder ejecutarla
+// exports.tarea2 = tarea2;
+
+const {src, dest, watch}= require('gulp')
+const sass = require ('gulp-sass')(require('sass'));
+
+function css(done){
+    src('src/scss/**/*.scss')//Identificar scss a compilar
+        .pipe(sass())//Compilar
+        .pipe(dest('build/css'))//Almacenar
+    done();
+}
+function dev (done){
+    watch('src/scss/**/*.scss', css);
+    done();
+}
+exports.css = css;
+exports.dev = dev;
